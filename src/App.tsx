20aec92be8886ab4b36be039de8cf15d536b2c6d
Alvaro Bianor Sousa Medeiros <bianor.alvaro12@gmail.com>
2021-04-04 14:57:03 -0300

    add global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyles } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
       <h1>Álvim Bianor Sousa Medeiros</h1>
+      <GlobalStyles />
     </div>
   );
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,3 +1,31 @@
-import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
 
-export const Container = styled.div``;
+export const GlobalStyles = createGlobalStyle`
+
+  :root {
+    --background: rgba(240,242,245,1.0);
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 92.75%;
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%;
+    }
+
+  }
+
+  body {
+    background-color: var(--background);
+    -webkit-font-smoothing: antialiased;
+  }
+
+`;
