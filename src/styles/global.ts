e6d2f8fb0d8650888ecd6ec57decc4aa8b96b33d
Alvaro Bianor Sousa Medeiros <bianor.alvaro12@gmail.com>
2021-04-04 15:07:11 -0300

    Styling fonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -38,6 +38,16 @@ export const GlobalStyles = createGlobalStyle`
     -webkit-font-smoothing: antialiased;
   }
 
+  body, input, text-area, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+
+  }
+
   button {
     cursor: pointer;
   }
