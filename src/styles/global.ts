f623b8b1716fb8d1424cf1afcadf323d87bcf24e
Alvaro Bianor Sousa Medeiros <bianor.alvaro12@gmail.com>
2021-04-04 14:57:21 -0300

    add global style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,6 +4,16 @@ export const GlobalStyles = createGlobalStyle`
 
   :root {
     --background: rgba(240,242,245,1.0);
+
+    --red: rgba(229,46,77,1.0);
+
+    --blue: rgba(84,41,204,1.0);
+    --blue-light: rgba(105,51,255,1.0);
+
+    --text-title: rgba(54,63,95,1.0);
+    --text-body: rgba(150,156,179,1.0);
+
+    --shape: rgba(255,255,255,1.0);
   }
 
   * {
@@ -26,6 +36,15 @@ export const GlobalStyles = createGlobalStyle`
   body {
     background-color: var(--background);
     -webkit-font-smoothing: antialiased;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
   }
 
 `;
